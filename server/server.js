95c0f32269cb6c6cc5c4910a87f11c62dcd0738b
solaries <solarinolakunle@yahoo.com>
2021-01-21 19:53:16 +0100

    add functionality to all application work well on heroku

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
+const serverStatic = require('serve-static');//
+const history = require('connect-history-api-fallback');//
+const path = require('path');//
+const enforce = require('express-sslify');//
 const express = require('express');
 
 const apiroutes = require('./routes');
 
-
-    const app = express();
-    app.use(express.json());
-    app.use("/api", apiroutes);
-    app.listen(process.env.PORT || 2244, () => {
-           console.log(`Listening on port: ${process.env.PORT || 2244}`);
-        }); 
+const app = express();
+app.use(enforce.HTTPS({ trustProtoHeader: true }));//
+app.use(serverStatic(path.join(__dirname, '/dist')));//
+app.use(history());//
+app.use(express.json());
+app.use('/api', apiroutes);
+app.listen(process.env.PORT || 2244, () => {
+  // console.log(`Listening on port: ${process.env.PORT || 2244}`);
+});
 
 function createServer() {
-    
-    return app;
-  }  
-  module.exports = createServer;
-  
+  return app;
+}
+module.exports = createServer;
